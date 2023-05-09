10df16b48a34445c8c8d68cfbd8d2bc427706b8e
Juan Carlos Fernández García <jcarlosjccm@gmail.com>
2023-05-09 10:04:59 +0200

    Terminado formularios template

--- src/app/template/pages/dynamic/dynamic.component.ts
+++ src/app/template/pages/dynamic/dynamic.component.ts
@@ -25,16 +25,53 @@ export class DynamicComponent {
 
   @ViewChild('myForm') myForm!: NgForm;
 
+  nuevoFavorito: string = '';
+
+
   persona: Persona = {
     nombre: 'Sonia',
     favoritos: [
       { id: 78787, nombre: 'Java' },
-      { id: 1245, nombre: 'Sql' }
+      { id: 1245, nombre: 'Sql' },
+      { id: 25, nombre: 'C++' }
     ]
   };
 
 
+
+  agregarFavorito() {
+  
+    // console.log( this.nuevoFavorito );
+    const favoritoNuevo: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoFavorito
+    };
+
+    this.persona.favoritos.push( favoritoNuevo );
+
+  }
+
+
+  eliminarFavorito( idx: number ) {
+  
+    this.persona.favoritos.splice( idx, 1 );
+  }
+
+
+
+
   guardar() {
+
+    
+
+    if ( this.myForm.invalid ) {
+      return;
+    }
+
+    console.log('Guardando...');
+
+
+
   }
 
 }
--- src/app/template/pages/switches/switches.component.ts
+++ src/app/template/pages/switches/switches.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class SwitchesComponent {
 
+
+  persona = {
+    genero: '',
+    notificaciones: false,
+  }
+
+  terminosYCondiciones = false;
+
+  guardar() {
+  
+    if ( !this.terminosYCondiciones ) {
+      console.log('Tienes que aceptar los términos y condiciones');
+      return;
+    }
+
+    console.log('Guardado');
+  
+  }
+
 }
