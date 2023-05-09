ba821186dbc5d78daa3f6b82c29e457bb388aaad
Juan Carlos Fernández García <jcarlosjccm@gmail.com>
2023-05-09 13:51:52 +0200

    Preparando validación asincrona email

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidationService } from '../../../shared/services/validation.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+
+  public miFormulario: FormGroup = this.fb.group({
+
+    nombre: [ '' , [ Validators.required , Validators.minLength(3) ] ],
+
+    identificador: [ '' , [ 
+
+        Validators.required,
+        Validators.pattern( this.validationService.expresion_regular_dni ),
+        this.validationService.validaDni
+      
+    ] ],
+
+    email: [ '' , [ 
+      Validators.required, 
+      Validators.pattern( this.validationService.emailPattern ) 
+    ]  ],
+
+    userName: [ '', [ Validators.required, ] ],
+
+
+    password1: [ '', [ Validators.required, Validators.minLength( 6 ) ]],
+
+    password2: [ '', [ Validators.required ]]
+
+
+  });
+
+  
+
+  constructor(private fb: FormBuilder,
+              private validationService: ValidationService ) { }
+
+
+  ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: 'Sonia',
+      identificador: '03869206P',
+    });
+
+  }
+
+
+
+  campoNoValido(campo: string ): boolean | undefined {
+    
+    return this.validationService.campoNoValido( this.miFormulario, campo);
+    
+  }
+
+
+
+
+  guardar() {
+  
+  }
+
 
 }
---
+++ src/app/shared/services/validation.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+
+
+
+  public emailPattern = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+  public expresion_regular_dni = /^(([X-Zx-z]{1})([-]?)(\d{7})([-]?)([A-Za-z]{1}))|((\d{8})([-]?)([A-Za-z]{1}))$/;
+
+
+  constructor() { }
+
+
+
+
+
+  campoNoValido(formulario: FormGroup, campo: string ): boolean | undefined {
+    
+    return formulario.get(campo)?.invalid 
+      && formulario.get(campo)?.touched;
+  }
+
+
+
+
+
+
+
+
+  validaDni ( control: FormControl ): ValidationErrors | null  {
+
+    const dni = control.value?.trim().toUpperCase() || ' ';
+
+    let numero, letraDni, letra;
+
+    numero = dni.substr(0,dni.length-1);
+    numero = numero.replace('X', 0);
+    numero = numero.replace('Y', 1);
+    numero = numero.replace('Z', 2);
+    letraDni = dni.substr(dni.length-1, 1);
+    numero = numero % 23;
+    letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
+    letra = letra.substring(numero, numero+1);
+    if (letra != letraDni) {
+        //alert('Dni erroneo, la letra del NIF no se corresponde');
+        return {
+          dni: 'incorrecto'
+        };
+    }else{
+        //alert('Dni correcto');
+        return null;
+    }
+
+  }
+
+
+
+
+
+}
